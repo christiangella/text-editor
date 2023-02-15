b98894206eb8d888dc9d0a6c20da95d95a815c69
christiangella <112913805+christiangella@users.noreply.github.com>
2023-02-15 15:01:09 -0800

    adding boilerplate items / db logic

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,25 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const name = 'jate'
+  const jateDb = await openDB('jate', 1)
+  const tx = jateDb.transaction(name, 'readonly')
+  const store = tx.objectStore(name)
+  const request = store.put({ content, id: 1})
+  const result = await request
+  console.log('data is saved', result)
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const name = 'jate'
+  const jateDb = await openDB('jate', 1)
+  const tx = jateDb.transaction(name, 'readwrite')
+  const store = tx.objectStore(name)
+  const request = store.getAll()
+  const result = await request
+  console.log('data retrieved', result)
+}
 
 initdb();
